a4f9713b496c1ea3bf3e2a932788d46aa7f3b428
菾凴 <buiawpkgew1@outlook.com>
2024-08-30 17:03:27 +0800

    Update 1.py

--- usage/1.py
+++ usage/1.py
@@ -32,10 +32,16 @@ jm_albums = '''
 465549
 463812
 '''
-# 去除空白字符并转换为列表
-jm_albums_list = [x.strip() for x in jm_albums.splitlines() if x.strip()]
-
-print(f"共 {jm_albums_list} 个本子需要下载。")
+# 去除空白字符并转换为集合去重，然后转换回列表
+jm_albums_set = {x.strip() for x in jm_albums.splitlines() if x.strip()}
+jm_albums_list = list(jm_albums_set)
+print(f"共jm_albums_set: {jm_albums_set}")
+print(f"共jm_albums_list: {jm_albums_list}")
+# 将去重后的结果保存到文件
+with open('unique_jm_albums.txt', 'w') as file:
+    for item in jm_albums_list:
+        file.write(f"{item}\n")
+print(f"结果已保存到 unique_jm_albums.txt 文件中。")
 print("##################")
 # 找到不在 list2 中的ID
 A = [x for x in jm_albums_list if x not in list2]
